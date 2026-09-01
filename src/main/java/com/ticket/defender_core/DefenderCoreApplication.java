d9af1558380e3dd1c247654baabcaab8838f425c
Minho <mhgeum07@gmail.com>
2026-09-01 19:41:08 +0900

    chore: Spring Boot 프로젝트 초기 환경 세팅 및 기본 파일 추가

---
+++ src/main/java/com/ticket/defender_core/DefenderCoreApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.defender_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefenderCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefenderCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticket/defender_core/DefenderCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.defender_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefenderCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
